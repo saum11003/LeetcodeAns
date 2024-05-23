dc38070ca8eb94c449872c94588ffe90240a6261
saum11003 <109400319+saum11003@users.noreply.github.com>
2024-05-23 10:16:33 +0530

    Time: 2 ms (77.71%), Space: 53.5 MB (6.62%) - LeetHub

---
+++ 0169-majority-element/0169-majority-element.java
@@ -0,0 +1,31 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int element=0;
+        int count =0;
+        
+        for(int i=0;i<nums.length;i++){
+            if(count ==0){
+                element=nums[i];
+                count=1;
+}else if(element==nums[i]){
+                count++;
+                
+            }else{
+                count--;
+            }
+        }
+        
+        int count1=0;
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==element){
+                count1++;
+            }
+        }
+        if(count1 > nums.length/2){
+            return element;
+}else{
+            return -1;
+        }
+        
+    }
+}

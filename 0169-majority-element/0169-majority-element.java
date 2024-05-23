d95774bc653797eacfbdb0b7c19863ba37313c5f
saum11003 <109400319+saum11003@users.noreply.github.com>
2024-05-23 10:20:57 +0530

    Time: 1 ms (99.81%), Space: 52.9 MB (43.58%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -15,17 +15,17 @@ class Solution {
             }
         }
         
-        int count1=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==element){
-                count1++;
-            }
-        }
-        if(count1 > nums.length/2){
+        // int count1=0;
+        // for(int i=0;i<nums.length;i++){
+        //     if(nums[i]==element){
+        //         count1++;
+        //     }
+        // }
+        // if(count1 > nums.length/2){
             return element;
-}else{
-            return -1;
-        }
+
+            // return -1;
+        
         
     }
 }

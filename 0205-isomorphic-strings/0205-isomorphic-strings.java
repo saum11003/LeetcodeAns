8f2ea661de7070a5b31f7d0f6b2d05e27d0b9107
saum11003 <109400319+saum11003@users.noreply.github.com>
2024-05-25 08:50:39 +0530

    Time: 16 ms (24.39%), Space: 42 MB (93.23%) - LeetHub

---
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -0,0 +1,37 @@
+import java.util.HashMap;
+
+public class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        if (s.length() != t.length()) {
+            return false;
+        }
+
+        HashMap<Character, Character> mapS = new HashMap<>();
+        HashMap<Character, Character> mapT = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char charS = s.charAt(i);
+            char charT = t.charAt(i);
+
+            if (mapS.containsKey(charS)) {
+                if (mapS.get(charS) != charT) {
+                    return false;
+                }
+            } else {
+                mapS.put(charS, charT);
+            }
+
+            if (mapT.containsKey(charT)) {
+                if (mapT.get(charT) != charS) {
+                    return false;
+                }
+            } else {
+                mapT.put(charT, charS);
+            }
+            
+        }
+        return true;
+    }
+
+        
+    }

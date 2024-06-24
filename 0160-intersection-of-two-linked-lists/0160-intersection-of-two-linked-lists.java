bc76d780ccfe2045aeaf9cbd448631fbba87522b
saum11003 <109400319+saum11003@users.noreply.github.com>
2024-06-24 12:06:46 +0530

    Time: 1 ms (99.86%), Space: 48.4 MB (48.38%) - LeetHub

--- 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
+++ 0160-intersection-of-two-linked-lists/0160-intersection-of-two-linked-lists.java
@@ -14,20 +14,59 @@ public class Solution {
         if(headA == null || headB == null){
             return null;
         }
-        HashMap<ListNode, Integer> vals = new HashMap<>();
+//         HashMap<ListNode, Integer> vals = new HashMap<>();
         
+//         ListNode tempA = headA;
+//         while(tempA != null){
+//             vals.put(tempA, 1);
+//             tempA = tempA.next;
+// }
+//         ListNode tempB = headB;
+//         while(tempB != null){
+//             if(vals.containsKey(tempB)){
+//                 return tempB;
+// }
+//             tempB = tempB.next;
+// }
+//         return null;
+        int cntA = 0;
         ListNode tempA = headA;
         while(tempA != null){
-            vals.put(tempA, 1);
             tempA = tempA.next;
-}
+            cntA++;
+        }
+        int cntB = 0;
         ListNode tempB = headB;
         while(tempB != null){
-            if(vals.containsKey(tempB)){
-                return tempB;
-}
             tempB = tempB.next;
-}
+            cntB++;
+        }
+        tempA= headA;
+        tempB = headB;
+        int val =0;
+        if(cntB>cntA){
+            val= cntB-cntA;
+            while(val != 0){
+                tempB= tempB.next;
+                val--;
+            }
+        }else if(cntA>cntB){
+            val=cntA-cntB;
+            while(val != 0){
+                tempA= tempA.next;
+                val--;
+            }
+        }else{
+            val=cntA-cntB;
+        }
+        while(tempA != null){
+            if(tempA ==tempB){
+                return tempA;
+            }
+            tempA = tempA.next;
+            tempB = tempB.next;
+        }
         return null;
+       
     }
 }

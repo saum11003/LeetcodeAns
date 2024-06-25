0217014ff0517ba6961c79c4a82ffedea1ad2db3
saum11003 <109400319+saum11003@users.noreply.github.com>
2024-06-25 17:50:32 +0530

    Time: 16 ms (28.50%), Space: 42.3 MB (43.34%) - LeetHub

--- 0205-isomorphic-strings/0205-isomorphic-strings.java
+++ 0205-isomorphic-strings/0205-isomorphic-strings.java
@@ -1,37 +1,31 @@
-import java.util.HashMap;
-
-public class Solution {
+class Solution {
     public boolean isIsomorphic(String s, String t) {
-        if (s.length() != t.length()) {
+        if(s.length()!=t.length()){
             return false;
         }
-
-        HashMap<Character, Character> mapS = new HashMap<>();
-        HashMap<Character, Character> mapT = new HashMap<>();
-
-        for (int i = 0; i < s.length(); i++) {
-            char charS = s.charAt(i);
-            char charT = t.charAt(i);
-
-            if (mapS.containsKey(charS)) {
-                if (mapS.get(charS) != charT) {
+        HashMap<Character,Character> mapS = new HashMap<>();
+        HashMap<Character,Character> mapT = new HashMap<>();
+        
+        for(int i =0;i<s.length();i++){
+            char S = s.charAt(i);
+            char T = t.charAt(i);
+            
+            if(mapS.containsKey(S)){
+                if(mapS.get(S)!=T){
                     return false;
                 }
-            } else {
-                mapS.put(charS, charT);
-            }
-
-            if (mapT.containsKey(charT)) {
-                if (mapT.get(charT) != charS) {
-                    return false;
-                }
-            } else {
-                mapT.put(charT, charS);
+            }else{
+                mapS.put(S,T);
             }
             
+            if(mapT.containsKey(T)){
+                if(mapT.get(T)!=S){
+                    return false;
+                }
+            }else{
+                mapT.put(T,S);
+            }
         }
         return true;
     }
-
-        
-    }
+}
